48026814754bf45d06b585817a1775087948e544
jpgroup600 <133649099+jpgroup600@users.noreply.github.com>
2024-11-26 01:59:29 +0900

    changed pagination backend as req qurety

--- Controllers/AdminControllers_Ayzenn/GetAllUsers.js
+++ Controllers/AdminControllers_Ayzenn/GetAllUsers.js
@@ -2,14 +2,18 @@ const UserModel = require('../../Models/User');
 
 const GetAllUsersFunction = async (req, res) => {
     try {
-        const page = parseInt(req.params.pagenumber) || 1; 
-        const limit = parseInt(req.params.limit) || 10; 
+        // Change from req.params to req.query
+        const page = parseInt(req.query.page) || 1; 
+        const limit = parseInt(req.query.limit) || 10; 
         const skip = (page - 1) * limit;
+
         const users = await UserModel.find({})
             .sort({ joinedDate: -1 })
             .skip(skip)
             .limit(limit);
+
         const totalUsers = await UserModel.countDocuments();
+
         res.status(200).json({
             totalUsers,
             totalPages: Math.ceil(totalUsers / limit),
@@ -20,7 +24,7 @@ const GetAllUsersFunction = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
 module.exports = {
     GetAllUsersFunction,
 };
-
